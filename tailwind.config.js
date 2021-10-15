cee90ba72c65d372b0d0e9d2325c7d4312e8a145
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-10-15 16:48:56 -0300

    💄Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
             './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
             './storage/framework/views/*.php',
             './resources/views/**/*.blade.php',
+            "./resources/js/**/*.js",
             './resources/views/hiperpreciostemplate/**/*.blade.php',
             './resources/views/hiperpreciostemplate/**/**/*.blade.php',
             './resources/views/hiperpreciostemplate/**/**/**/*.blade.php',
