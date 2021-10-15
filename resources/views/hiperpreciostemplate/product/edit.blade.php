c63b07322be881e82a81b54f49c1ae4c9b8fd43b
Juan M. Cortéz <juanm.cortez@gmail.com>
2021-10-15 16:47:55 -0300

    💄 "Product" component views update

--- resources/views/hiperpreciostemplate/product/edit.blade.php
+++ resources/views/hiperpreciostemplate/product/edit.blade.php
@@ -3,13 +3,14 @@
     <x-slot name="title">{!! $title !!}</x-slot>
     <x-slot name="description">{!! $description !!}</x-slot>
     {{-- Page Title --}}
-    <h1 class="py-4 px-6 text-xl font-bold text-gray-700 leading-loose">{!! $title !!}</h1>
+    <h1 class="px-6 py-4 text-xl font-bold leading-loose text-gray-700">{!! $title !!}</h1>
     {{-- Page Content --}}
     <form method="POST" action="{{ route('products.update', ['product' => $product->id]) }}"
         enctype="multipart/form-data">
         @csrf
         @method('POST')
-        <x-tables.product_edit :categorySelect="$categories" :unitSelect="$units" :columnContents="$product" />
+        <x-tables.product_edit :brandSelect="$brands" :categorySelect="$categories" :unitSelect="$units"
+            :columnContents="$product" />
 
         <x-tables.rows.savecancel routeName="products" paramName="product" itemId="{{ $product->id }}" />
     </form>
